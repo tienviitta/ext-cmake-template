e45c8b68aca2bde95a43ddf658ebd90b3f57be95
Väisänen, Petri <petri.vaisanen@nordicsemi.no>
2023-11-23 14:18:29 +0200

    Some comments added and editorial changes

--- apps/app.cpp
+++ apps/app.cpp
@@ -8,10 +8,7 @@
 
 int main() {
     std::vector<double> input = {1.2, 2.3, 3.4, 4.5};
-
     auto [mean, stddev] = accumulate_vector(input);
-
     fmt::print("Mean: {}, Stddev: {}\n", mean, stddev);
-
     return 0;
 }
